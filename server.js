c72e3a84e6ac2678356c6f211ff1aeef5f87a370
ShreyaKumari0911 <shreyaaasingh8@gmail.com>
2023-10-08 16:40:42 +0530

    server config added and js added

--- server.js
+++ server.js
@@ -1,57 +1,74 @@
-const http  = require('http');//http module
-const fs = require('fs');
+
+// Requiring in-built https for creating 
+// https server 
+const https = require("https"); 
+  
+// Express for handling GET and POST request 
+const express = require("express"); 
+const app = express(); 
+
+// Requiring file system to use local files 
+const fs = require("fs"); 
 const path = require('path');
-//creating a server 
 
-const server = http.createServer((req,res)=>{
-    console.log('request has been made from browser to server!');
-    console.log(req);
-    console.log(req.method);
-    console.log(req.url);
-    //creating responds from server when a request is made
-    res.setHeader('Content-Type','text/html');
-    // res.write("Hello From first server!");
-    // res.write("<h1>Hello From first server!</h1>");
-    // res.write("Welcome to SwapShop!");
+const port = 8000;
 
-    let path = '/';
-    // adding the routes in website
+// Configuring express to use body-parser 
+// as middle-ware 
+const mime = require('mime');
+// Serve static files (e.g., HTML, CSS, JavaScript) from a directory
+app.use(express.static('public'));
+app.use(express.static(__dirname));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-    switch(req.url){
-        case '/':
-            path+='/index.html'
-            break;
-        case '/aboutus':
-            path+='/aboutus.html'
-            break;
-        case '/blog':
-            path+='/blog.html'
-            break;
-        case '/cart':
-            path+='/cart.html'
-            break;
-        case '/contact':
-            path+='/contact.html'
-            break;
-        default:
-            path+='/404.html'
-            break;
-    }
-    fs.readFile(path,(err,fileData)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            // res.write(fileData);
-            res.end(fileData);
-        }
-    });
 
+app.get("/", (req, res)=>{
+    console.log(__dirname+"/public/index.html");
+    const filePath = path.resolve(__dirname, 'index.html');
+    res.sendFile(filePath);
+})
+
+app.get("/cart", (req, res)=>{
+    console.log(__dirname+"/public/cart.html");
+    res.sendFile(__dirname + "/public/cart.html");
+})
+
+app.get("/login", (req, res)=>{
+    console.log(__dirname+"/public/login.html");
+    res.sendFile(__dirname + "/public/login.html");
+})
+
+app.get("/shop", (req, res)=>{
+    console.log(__dirname+"/public/shop.html");
+    res.sendFile(__dirname + "/public/shop.html");
+})
+
+app.get("/contact", (req, res)=>{
+    console.log(__dirname+"/public/contact.html");
+    res.sendFile(__dirname + "/public/contact.html");
+})
+
+app.get("/login_google", (req, res)=>{
+    console.log(__dirname+"/public/login_google.html");
+    res.sendFile(__dirname + "/public/login_google.html");
+})
+
+app.get("/signup", (req, res)=>{
+    console.log(__dirname+"/public/signup.html");
+    res.sendFile(__dirname + "/public/signup.html");
+})
+
+app.get("/blog", (req, res)=>{
+    console.log(__dirname+"/public/blog.html");
+    res.sendFile(__dirname + "/public/blog.html");
+})
+
+app.get("/404", (req, res)=>{
+    console.log(__dirname+"/public/404.html");
+    res.sendFile(__dirname + "/public/404.html");
+})
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`)
 });
-
-//port numnber and host
-server.listen(3000,'localhost',()=>{
-    console.log("server is running on port 3000");
-});
-
-
